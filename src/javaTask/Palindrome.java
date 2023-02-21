77f5feaca2a2f0f203879cbabeb78325c71b11e9
bahodurotayev <tjkbaha@gmail.com>
2023-02-20 20:07:00 -0500

    TestNg

--- src/javaTask/Palindrome.java
+++ src/javaTask/Palindrome.java
@@ -4,17 +4,34 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Palindrome {
-    public static void main(String[] args) {
-        boolean palindrome = isPalindrome(11211);
-        System.out.println(palindrome);
+    @Test
+    void isNumberPalindrome(){
+        isPalindrome(11211);
     }
-    static boolean isPalindrome(int target){
+    static void isPalindrome(int target){
+
         int number = target;
         int reverse = 0;
         while (number > 0){
         reverse = (reverse * 10) + number % 10;
         number = number/ 10;
     }
-        return reverse == target;
+        Assert.assertEquals(target,reverse, "They are not palindrome");
     }
+
+    static void isPalindrome(String str){
+        int start = str.charAt(0);
+        int end = str.charAt(str.length()-1);
+
+        while (start < end){
+            if (str.charAt(start) == str.charAt(end)){
+                start++;
+                end--;
+            }else {
+                break;
+            }
+        }
+    }
+
+
 }
